c52e854786cbeb5469ca9dfaf2aa456407635672
Josh Lowe <joshlowe.cs@gmail.com>
2025-11-23 15:49:02 -0500

    Implement high-level architecture for MIA

---
+++ mia_mitigation_in_fed_learning/mia.py
@@ -0,0 +1,21 @@
+def load_model():
+    pass
+
+def prepare_data():
+    pass
+
+def run_mia():
+    pass
+
+def save_results():
+    pass
+
+
+def main():
+    load_model()
+    prepare_data()
+    run_mia()
+    save_results()
+
+if __name__ == "__main__":
+    main()
